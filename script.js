7d04adaad0bb1e63c3ad9e0316612a614a9b0624
yungestAmine <purpleamine@gmail.com>
2023-07-02 13:23:47 +0200

    gestita parte 1p

--- script.js
+++ script.js
@@ -70,9 +70,14 @@ var paroleSegrete = [
   "zaino",
 ]
 var parolaSegreta = paroleSegrete[Math.floor(Math.random() * paroleSegrete.length)];
+
+console.log(parolaSegreta)
+
 //######################## GLOBAL VARIABLES ###########################à
 let body = document.querySelector('body')
 let main = document.querySelector('main')
+let possibilita = 9
+
 function start(){
     main.style.backgroundImage = "url(./img/wall.jpg)"
     let h1 = document.createElement('h1')
@@ -103,20 +108,77 @@ function start(){
     }
     main.appendChild(div)
 }
-function start_game(){
-    
+function start_game(img){
+    switch(img.id){
+        case 'img_0':
+            main.innerHTML = ""
+        break;
+        case 'img_1':
+            main.innerHTML = ""
+            let h1 = document.createElement('h1')
+            h1.textContent = 'IMPICCATO'
+            let div = document.createElement('div')
+            div.id = 'div_2'
+            let mini_div_1 = document.createElement('div')
+            mini_div_1.id = 'mini_div_1'
+            let h4 = document.createElement('h4')
+            h4.textContent = 'barra tentativi restanti'
+            h4.style.textAlign = 'start'
+            for(let i=0; i<9; ++i){
+                let possibilita = document.createElement('div')
+                possibilita.classList.add('possibilita')
+                mini_div_1.appendChild(possibilita)
+            }
+            div.appendChild(mini_div_1)
+            main.appendChild(h1)
+            main.appendChild(h4)
+            main.appendChild(div)
+        
+            let h3 = document.createElement('h3')
+            h3.textContent = 'parola segreta'
+            h3.style.textAlign = 'center'
+            div.appendChild(h3)
+            let mini_div_2 = document.createElement('div')
+            mini_div_2.id = 'mini_div_2'
+            for(let i=0; i<parolaSegreta.length; ++i){
+                let cas = document.createElement('div')
+                cas.classList.add('cas')
+                if(i==0)
+                  cas.textContent = parolaSegreta[0]
+                mini_div_2.appendChild(cas)
+            }
+            div.appendChild(mini_div_2)
+            let input = document.createElement('input')
+            input.id = 'input'
+            div.appendChild(input)
+            let value = ''
+            let fnd = false
+            let cass = document.getElementsByClassName('cas')
+            let possibilit = document.getElementsByClassName('possibilita')
+            input.addEventListener('keydown', (event)=>{
+                if(event.keyCode === 13)
+                    value = input.value
+                    for(let i=0; i<parolaSegreta.length; ++i)
+                    if(value===parolaSegreta[i]){
+                        cass[i].textContent = value
+                        fnd = true   
+                    }
+                if(!fnd)
+                {
+                    possibilita--
+                    possibilit[possibilita].style.visibility = 'hidden'
+                }
+            })
+            input.value = ''
+            break;
+        }
+            
 }
 // console.log(parolaSegreta.length)
 // let ct = document.getElementsByClassName('ro')[0]
-// for(let i=0; i<parolaSegreta.length; ++i){
-//       let cas = document.createElement('div')
-//       cas.classList.add('cas')
-//       if(i==0)
-//           cas.textContent = parolaSegreta[0]
-//       ct.appendChild(cas)
-//     }
+// 
 // let possibilita = 9
-// let fnd = false
+// 
 // function ver(){
 //     fnd = false
 //     let cass = document.getElementsByClassName('cas')
