f2b975afe0f409e9e095d116df16499a2351b8c7
yungestAmine <purpleamine@gmail.com>
2023-08-31 16:16:52 +0200

    mi son svegliato dopo 1 mese

---
+++ img/Automate-Text-Bombing-main/Automate-Text-Bombing-main/Bomber_Source_Code.py
@@ -0,0 +1,28 @@
+import time # importing time module
+import pyautogui # importing pyautogui
+
+T = 0 # Desired Time before sending each messages
+Text = "fflush(stdin)"
+Time = 999999 # How many messages the user want to send
+
+# I'll use while loop here
+
+i = 0
+
+while (i <= Time-1 ): # The loop started
+    time.sleep(T) # will wait T times before sending each automated mesfflush(stdin)
+    fflush(stdin)
+    fflush(stdin)
+    fflush(stdin)
+    fflush(stdin)
+    fflush(stdin)
+    fflush(stdin)
+    fflush(stdin)
+    fflush(stdin)
+    fflush(stdin)
+    fflush(stdin)
+    fflush(stdin)
+    fflush(stdin)sage
+    pyautogui.typewrite(Text) # will write the text the user want
+    pyautogui.press('enter') # will work as the 'Enter' button
+    i+=1 # i will increment as 1 each time
--- script.js
+++ script.js
@@ -69,132 +69,332 @@ var paroleSegrete = [
   "nave",
   "zaino",
 ]
-var parolaSegreta = paroleSegrete[Math.floor(Math.random() * paroleSegrete.length)];
 
-console.log(parolaSegreta)
+var paroleSegretee = [
+    "ciao",
+  "sole",
+  "cane",
+  "gatto",
+  "casa",
+  "mare",
+  "cielo",
+  "auto",
+  "day",
+  "notte",
+  "festa",
+  "amico",
+  "felice",
+  "banana",
+  "garden",
+  "libro",
+  "musica",
+  "mela",
+  "computer",
+  "stella",
+  "bosco",
+  "hot",
+  "freddo",
+  "gioco",
+  "pranzo",
+  "pasta",
+  "pizza",
+  "icecream",
+  "torta",
+  "calcio",
+  "basket",
+  "tennis",
+  "swimming",
+  "corsa",
+  "school",
+  "studio",
+  "lezione",
+  "pen",
+  "matita",
+  "quaderno",
+  "telefono",
+  "clock",
+  "camicia",
+  "pantalone",
+  "scarpe",
+  "borsa",
+  "lampada",
+  "settings",
+  "martello",
+  "cameriere",
+  "programma",
+  "umbrella",
+  "coat",
+  "tastiera",
+  "torre",
+  "bicicletta",
+  "candel",
+  "vento",
+  "radio",
+  "telefono",
+  "carta",
+  "luna",
+  "juice",
+  "cartella",
+  "window",
+  "anatra",
+  "nave",
+  "backpack",
+]
+var parolaSegreta = paroleSegrete[Math.floor(Math.random() * paroleSegrete.length)];
+var parolaSegretaa = paroleSegretee[Math.floor(Math.random() * paroleSegretee.length)];
+ 
 
 //######################## GLOBAL VARIABLES ###########################à
 let body = document.querySelector('body')
 let main = document.querySelector('main')
-let possibilita = 9
+let possibilita=6
 
-function start(){
-    main.style.backgroundImage = "url(./img/wall.jpg)"
+
+function play(mode){
+    console.log(mode)
+    main.innerHTML = ""
     let h1 = document.createElement('h1')
-    let p = document.createElement('h3')
-    let div = document.createElement('div')
-    h1.textContent = 'Benvenuto sfidante'
-    p.textContent = 'Scegli la modalità di gioco'
-    main.appendChild(h1)
-    main.appendChild(p)
-    div.id = 'div_1'
-    for(let i=0; i<2; i++){
-        let mini_div = document.createElement('div')
-        mini_div.classList.add('mini_div')
-        div.appendChild(mini_div)
-        if(i==0){
-            let img = document.createElement('img')
-            img.src = './img/0.png'
-            img.id = 'img_0'
-            mini_div.appendChild(img)
-            img.onclick = function() {start_game(this)};
-        }else{
-            let img = document.createElement('img')
-            img.src = './img/1.png'
-            img.id = 'img_1'
-            mini_div.appendChild(img)
-            img.onclick = function() {start_game(this)};
-        }
-    }
-    main.appendChild(div)
-}
-function start_game(img){
-    switch(img.id){
-        case 'img_0':
-            main.innerHTML = ""
-        break;
-        case 'img_1':
-            main.innerHTML = ""
-            let h1 = document.createElement('h1')
-            h1.textContent = 'IMPICCATO'
-            let div = document.createElement('div')
-            div.id = 'div_2'
-            let mini_div_1 = document.createElement('div')
-            mini_div_1.id = 'mini_div_1'
-            let h4 = document.createElement('h4')
-            h4.textContent = 'barra tentativi restanti'
-            h4.style.textAlign = 'start'
-            for(let i=0; i<9; ++i){
-                let possibilita = document.createElement('div')
-                possibilita.classList.add('possibilita')
-                mini_div_1.appendChild(possibilita)
-            }
-            div.appendChild(mini_div_1)
-            main.appendChild(h1)
-            main.appendChild(h4)
-            main.appendChild(div)
-        
-            let h3 = document.createElement('h3')
-            h3.textContent = 'parola segreta'
-            h3.style.textAlign = 'center'
-            div.appendChild(h3)
-            let mini_div_2 = document.createElement('div')
-            mini_div_2.id = 'mini_div_2'
-            for(let i=0; i<parolaSegreta.length; ++i){
-                let cas = document.createElement('div')
-                cas.classList.add('cas')
-                if(i==0)
-                  cas.textContent = parolaSegreta[0]
-                mini_div_2.appendChild(cas)
-            }
-            div.appendChild(mini_div_2)
-            let input = document.createElement('input')
-            input.id = 'input'
-            div.appendChild(input)
-            let value = ''
-            let fnd = false
-            let cass = document.getElementsByClassName('cas')
-            let possibilit = document.getElementsByClassName('possibilita')
-            input.addEventListener('keydown', (event)=>{
-                if(event.keyCode === 13)
-                    value = input.value
-                    for(let i=0; i<parolaSegreta.length; ++i)
-                    if(value===parolaSegreta[i]){
-                        cass[i].textContent = value
-                        fnd = true   
-                    }
-                if(!fnd)
-                {
-                    possibilita--
-                    possibilit[possibilita].style.visibility = 'hidden'
-                }
-            })
-            input.value = ''
-            break;
-        }
-            
-}
-// console.log(parolaSegreta.length)
-// let ct = document.getElementsByClassName('ro')[0]
-// 
-// let possibilita = 9
-// 
-// function ver(){
-//     fnd = false
-//     let cass = document.getElementsByClassName('cas')
-//     let str = document.querySelector('input')
 
-//     for(let i=0; i<parolaSegreta.length; ++i)
-//         if(str.value===parolaSegreta[i]){
-//             cass[i].textContent = str.value
-//             fnd = true   
-//         }
-//     if(!fnd)
-//     {
-//         possibilita--
-//         document.getElementById('possibilita').textContent = possibilita.toString()
-//     }
-//     if(str.value === parolaSegreta)
-//         document.getElementById('possibilita').textContent = 'HAI VINTO'
-//     str.value = ""
-// }
+    switch(mode.id){
+        case 'I':
+            h1.textContent = "scegli il tuo nickname..."
+            let input = document.createElement('input')
+            input.id = "chsNck"
+            input.placeholder = " inserisci il nome"
+            let btn = document.createElement('button')
+            btn.id = "avanti"
+            btn.textContent = "avanti"
+            main.appendChild(h1)
+            main.appendChild(input)
+            main.appendChild(btn)
+            btn.onclick = function() {start(input, mode.id)} 
+        break;
+        case 'II':
+        break;
+    }
+}
+function start(name, players){
+     
+    if(name.value == "")
+    {
+        name.style.backgroundColor = "#c16e6e"
+        name.focus()
+        alert("inserisci un nome")
+    }else{
+        main.innerHTML = ""
+        let h1 = document.createElement('h1')  
+        h1.textContent = "ultimi dettagli..."
+        let h3 = document.createElement('h3')
+        h3.textContent = "vite giocatore"
+        let h3i = document.createElement('h3')
+        h3i.textContent = "evidenzia vocali"
+        let h3b = document.createElement('h3')
+        h3b.textContent = "parole straniere"
+
+        let div1 = document.createElement('div')
+        let div2 = document.createElement('div')
+        let div3 = document.createElement('div')
+
+        ///DIV1 AREA
+        let vite = document.createElement('h3')
+        vite.textContent = "6"
+        let divVite = document.createElement('div')
+        let btn1 = document.createElement('button')
+        let btn2 = document.createElement('button')
+
+        btn1.textContent = "-"
+        btn2.textContent = "+"
+
+        btn1.onclick = function() {upAndDown(this, vite)}
+        btn2.onclick = function() {upAndDown(this, vite)}
+
+        btn1.style.margin = "10px"
+        btn2.style.margin = "10px"
+        btn1.style.width = "25px"
+        btn2.style.width = "25px"
+        btn1.style.fontSize = "1.5rem"
+        btn2.style.fontSize = "1.5rem"
+
+        divVite.appendChild(btn1)
+        divVite.appendChild(vite)
+        divVite.appendChild(btn2)
+        divVite.id = "divDelCazzo"
+
+        div1.appendChild(h3)
+        div1.appendChild(divVite)
+        div1.id = "div1"
+
+
+        ///DIV2 AREA
+        let flagVocali = document.createElement('h3')
+        flagVocali.textContent = "no"
+
+        let divVocali = document.createElement('div')
+        let btn1v = document.createElement('button')
+        let btn2v = document.createElement('button')
+
+        btn1v.textContent = "<"
+        btn2v.textContent = ">"
+
+        btn1v.onclick = function() {yesOrNot(this, flagVocali)}
+        btn2v.onclick = function() {yesOrNot(this, flagVocali)}
+
+        btn1v.style.margin = "10px"
+        btn2v.style.margin = "10px"
+        btn1v.style.width = "25px"
+        btn2v.style.width = "25px"
+        btn1v.style.fontSize = "1.5rem"
+        btn2v.style.fontSize = "1.5rem"
+
+        divVocali.appendChild(btn1v)
+        divVocali.appendChild(flagVocali)
+        divVocali.appendChild(btn2v)
+        divVocali.id = "divDelCazzo"
+
+        div2.appendChild(h3i)
+        div2.appendChild(divVocali)
+        div2.id = "div1"
+
+        ///DIV3
+        let flagStraniera = document.createElement('h3')
+        flagStraniera.textContent = "no"
+
+        let divStraniere = document.createElement('div')
+        let btn1f = document.createElement('button')
+        let btn2f = document.createElement('button')
+
+        btn1f.textContent = "<"
+        btn2f.textContent = ">"
+
+        btn1f.onclick = function() {yesOrNot(this, flagStraniera)}
+        btn2f.onclick = function() {yesOrNot(this, flagStraniera)}
+
+        btn1f.style.margin = "10px"
+        btn2f.style.margin = "10px"
+        btn1f.style.width = "25px"
+        btn2f.style.width = "25px"
+        btn1f.style.fontSize = "1.5rem"
+        btn2f.style.fontSize = "1.5rem"
+
+        divStraniere.appendChild(btn1f)
+        divStraniere.appendChild(flagStraniera)
+        divStraniere.appendChild(btn2f)
+        divStraniere.id = "divDelCazzo"
+
+        div3.appendChild(h3b)
+        div3.appendChild(divStraniere)
+        div3.id = "div1"
+
+        let h2 = document.createElement('h2')
+        h2.textContent = "iniziamo..."
+        h2.id = "h2o"
+
+        h2.onclick = function() {game(vite.textContent, flagVocali.textContent, flagStraniera.textContent, name, )}
+
+        main.appendChild(h1)
+        main.appendChild(div1)
+        main.appendChild(div2)
+        main.appendChild(div3)
+        main.appendChild(h2)
+
+    }
+}
+function upAndDown(btn, nVite){
+    switch(btn.textContent){
+        case "-":
+            if(parseInt(nVite.textContent) > 0){
+                let a = parseInt(nVite.textContent)-1
+                nVite.textContent = a
+            }
+        break;
+        case "+":
+            if(parseInt(nVite.textContent)<15){
+                let a = parseInt(nVite.textContent)+1
+                nVite.textContent = a
+            }
+        break;
+    }
+}
+function yesOrNot(btn, nVite){
+    if(nVite.textContent == "no")
+        nVite.textContent = "si"
+    else
+        nVite.textContent = "no"
+}
+
+function game(nVite, voc, stran, nick){
+    possibilita = parseInt(nVite)
+    let h1 = document.createElement('h1')
+    let s="";
+    for(let i=0; i<parseInt(nVite); ++i){
+        s+="♥"
+    }
+    h1.textContent = nick.value + ": "+s
+    main.innerHTML = ""
+    let divWord = document.createElement('div')
+    divWord.id = "divWord"
+    let x
+    if(stran == "si")
+        x = parolaSegretaa
+    else
+        x = parolaSegreta
+    for(let i=0; i<x.length; ++i){
+        let div = document.createElement('div')
+        div.classList.add('words')
+        if(voc == "si"){
+            if(x[i] == "a" || x[i] == "e" || x[i] == "i" || x[i] == "o" || x[i] == "u" )
+            div.style.border = "2px solid red"
+        }
+        divWord.appendChild(div)
+        
+    }
+    let input = document.createElement('input')
+    input.id = "chsNck"
+    input.placeholder = " indovina"
+    input.onkeydown = function() {ver(event, this, h1, nick, nVite, x)}
+    main.appendChild(h1)
+    main.appendChild(divWord)
+    main.appendChild(input)
+}
+function ver(e, input, h1, nick, nVite, x){
+    let w = document.getElementsByClassName('words')
+    let found = false, i;
+    if(e.key == "Enter" && input.value!=""){
+        for(i=0; i<x.length; i++ ){
+            if(input.value == x[i]){
+                found = true;
+                w[i].textContent = input.value
+                input.value=""
+            }else if(input.value == x){
+                alert("hai vinto")
+                return
+            }
+        }
+        if(!found)
+        {
+            input.value=""
+            possibilita--
+            let s=""
+            for(let i=0; i<possibilita; ++i){
+                s+="♥"
+            }
+            h1.textContent = nick.value + ": "+s
+        }
+        if(possibilita == 0){
+            alert("hai perso! la parola era: "+x)
+            window.location.reload();
+        }
+        let end = true
+        w.forEach(sq => {
+            if(sq.textContent == "")
+                end = false
+        });
+        if(!end){
+            alert("hai perso!")
+            window.location.reload()
+        }
+
+        
+    }
+    
+}
